f7d360daae5e09dacad8c10c8e381d84062f5d52
pch <pch@7d6175ef-f675-4c24-bb17-d6b158afa8cd>
2009-05-04 22:09:41 +0000

    Put back SetupConstants.h -- shouldn't have baleeted it!

---
+++ src/SetupConstants.h
@@ -0,0 +1,24 @@
+/*
+ *  SetupConstants.h
+ *  TROGDOR
+ *
+ *  Created by Paul Hansen on 6/13/08.
+ *  Copyright 2008 Stanford University. All rights reserved.
+ *
+ *  $Rev:: 17                            $:  Revision of last commit
+ *  $Author:: pch                        $:  Author of last commit
+ *
+ *  $Date: 2009-02-05 14:07:45 -0800 (Thu, 05 Feb 2009) $:
+ *  $Id: SetupConstants.h 17 2009-02-05 22:07:45Z pch $:
+ *
+ */
+
+#ifndef _SETUPCONSTANTS_
+#define _SETUPCONSTANTS_
+
+enum FillStyle { kPECStyle, kPMCStyle, kHalfCellStyle };
+enum Field { kElectric, kMagnetic };
+enum TFSFType { kNoType, kTFType, kSFType };
+enum TFSFBufferType { kLinkType, kFileType };		
+
+#endif
