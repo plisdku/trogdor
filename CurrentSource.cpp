a8d69f8f44189b815a1c130da86257b276252638
pch <pch@7d6175ef-f675-4c24-bb17-d6b158afa8cd>
2009-09-16 23:43:13 +0000

    debug stuff

--- CurrentSource.cpp
+++ CurrentSource.cpp
@@ -166,6 +166,8 @@ CurrentSource(const CurrentSourceDescPtr & description,
     // We'll allocate things later.  For now, let's make maps of offsets
     // for each material.
     
+    for (int mm = 0; mm < mMaterialIDs.size(); mm++)
+        LOG << mMaterialIDs[mm] << endl;
     for (int xyz = 0; xyz < 3; xyz++)
     {
         long offset = 0;
